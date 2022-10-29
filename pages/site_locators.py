da142a6bc24acd6d03bd02a3a8fc93fa8d7e57c1
AdamOsses <adamosses@users.noreply.github.com>
2022-10-30 00:51:08 +0200

    new locators

--- pages/site_locators.py
+++ pages/site_locators.py
@@ -5,3 +5,13 @@ class SiteLocators:
     # Whole site locators
     PRODUCTS_STORE = (By.ID, "nava")
     HOME = (By.XPATH, '//a[@class="navbar-brand"]')
+    CONTACT = (By.XPATH, '//*[@data-target="#exampleModal"]')
+    ABOUT_US = (By.XPATH, '//*[@data-target="#videoModal"]')
+    CART = (By.ID, "cartur")
+    LOG_IN = (By.ID, "login2")
+    SIGN_UP = (By.ID, "signin2")
+    LOG_OUT = (By.ID, "logout2")
+    WELCOME_USER = (By.ID, "nameofuser")
+
+# login: ahk
+# password: ahk
