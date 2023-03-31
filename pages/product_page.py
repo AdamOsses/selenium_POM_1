3e94ef47f2936bf2407a2fb8084cb3a88ee9d170
AdamOsses <adamosses@users.noreply.github.com>
2023-03-31 22:52:39 +0200

    test_name_price_desc_are_equal

--- pages/product_page.py
+++ pages/product_page.py
@@ -19,5 +19,20 @@ class ProductPage(BasePage):
             # Page didn't load propretly.
             return False
 
+    def get_product_data(self):
+        image = self.driver.find_element(*ProductPageLocators.PRODUCT_IMAGE)
+        name = self.driver.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        description = self.driver.find_element(*ProductPageLocators.PRODUCT_DESCRIPTION).text
+        description = description.replace('Product description\n','')  # remove header
+        price = self.driver.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        price = price.replace(' *includes tax', '')
+        product_data = {'image': image,
+                        'name': name,
+                        'description': description,
+                        'price': price
+        }
+        return product_data
+
+
     def click_add_to_cart_button(self):
         pass
--- tests/product_page_test.py
+++ tests/product_page_test.py
@@ -12,3 +12,22 @@ class ProductPageTest(BaseTest):
         # Pages used by tests
         self.home_page = HomePage(self.driver)
         self.product_page = ProductPage(self.driver)
+
+    def test_name_price_desc_are_equal(self):
+        # check if name, price, desc. are equal on main page and product page
+        selected_product = self.home_page.get_product_data_by_number(0)
+        selected_product['product_href'].click()
+        data_on_product_page = self.product_page.get_product_data()
+        name_equal_t_f = selected_product['product_name'] == data_on_product_page['name']
+        price_equal_t_f = selected_product['product_price'] == data_on_product_page['price']
+        description_equal_t_f = selected_product['product_description'] == data_on_product_page['description']
+        # print(name, price, description, sep = '\n----------\n')
+        all_t_f = name_equal_t_f and price_equal_t_f and description_equal_t_f
+        # if one or more elements are not equal test will fail
+        self.assertTrue(all_t_f, f'Same name = {name_equal_t_f}, '
+                                 f'same price = {price_equal_t_f}, '
+                                 f'same descr. = {description_equal_t_f}')
+
+
+
+
